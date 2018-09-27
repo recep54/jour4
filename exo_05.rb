8471a4fe433af28f59f8ad9554d5ad1650128ec6
recep54 <recep54@outlook.fr>
2018-09-27 11:36:30 +0200

    exo 5

---
+++ exo_05.rb
@@ -0,0 +1,31 @@
+#ça va nous afficher On va compter le nombre d'heures de travail à THP
+puts "On va compter le nombre d'heures de travail à THP"
+#afficher tavail et faire le calul de 10*5*11
+puts "Travail : #{10 * 5 * 11}"
+#afficher début de la phrase et faire le calcul de 10*5*11*60
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
+#afficher "Et en secondes ?"
+puts "Et en secondes ?"
+#afficher le calcul de 10*5*11*60*60
+puts 10 * 5 * 11 * 60 * 60
+#afficher la phrase tel quel l'est "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+
+#va nous afficher false car 3 + 2 > 5-7
+puts 3 + 2 < 5 - 7
+
+#afficher la phrase et calculer le resultat entre accolade
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+#afficher la phrase et calculer le resultat entre accolade
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+#afficher la phrase entre ""
+puts "Ok, c'est faux alors !"
+#afficher la phrase entre ""
+puts "C'est drôle ça, faisons-en plus :"
+
+#afficher la phrase et puis true ou false selon les conditions dans les accolades
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
+
+# #{} fait excute la tache dedans 
