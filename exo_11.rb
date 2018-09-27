3650a60e8065c64a7a426ea862a24b0723b9c9f6
recep54 <recep54@outlook.fr>
2018-09-27 13:28:30 +0200

    exo 11

---
+++ exo_11.rb
@@ -0,0 +1,8 @@
+puts " Choississez un nombre, une petite suprise vous attend, quel est votre nombre choisit ?"
+print ">"
+num = gets.to_i
+
+num.times do
+	puts "Salut, ça farte ?"
+end
+puts "cool nan !!"
