9f2463af3ee7f2a5d2b70963cc7b53ed72778204
recep54 <recep54@outlook.fr>
2018-09-27 12:15:50 +0200

    exo 10

---
+++ exo_10.rb
@@ -0,0 +1,8 @@
+puts " Quels est l'année de votre naissance ?"
+print ">"
+user_year = gets.to_i
+user_age_in_2007 = 2007 - user_year
+
+puts "Bonjour, en 2007 vous avez eu #{user_age_in_2007} ans !"
+
+
