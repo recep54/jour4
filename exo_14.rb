a6ab0087cb7b50b11898b6dd274dd732e2bc8925
recep54 <recep54@outlook.fr>
2018-09-27 13:46:12 +0200

    exo 14

---
+++ exo_14.rb
@@ -0,0 +1,10 @@
+puts " Choississez un nombre, une petite suprise vous attend, quel est votre nombre choisit ?"
+print ">"
+num = gets.to_i
+
+num.times do 
+	puts num
+	num -= 1
+end
+puts "0"
+puts "C'est rigolo nan"
