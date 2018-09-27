8aeeb858bb4d85a62ca675c524d237cb633ef8ce
recep54 <recep54@outlook.fr>
2018-09-27 13:31:35 +0200

    exo 12

---
+++ exo_12.rb
@@ -0,0 +1,10 @@
+puts " Choississez un nombre, une petite suprise vous attend, quel est votre nombre choisit ?"
+print ">"
+num = gets.to_i
+
+i = 0
+num.times do
+	i += 1
+	puts i 
+end
+puts "cool nan !!"
